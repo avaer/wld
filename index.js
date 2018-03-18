4b504fdc25dae3a4a2e2c380bdb045c7cd511920
Avaer Kazmer <a@modules.io>
2018-03-18 00:51:34 -0700

    More index.js work

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const util = require('util');
 const parse5 = require('parse5');
 const selector = require('selector-lite');
 
@@ -7,14 +8,16 @@ if (require.main === module) {
   if (fileName) {
     fs.readFile(fileName, err => {
       if (!err) {
-        const document = parse5.parse(fileName);
-        console.log('got document', document);
+        const document = parse5.parse(fileName); // XXX need to spit out actual HTMLElement objects
+        const html = document.childNodes.find(el => el.tagName === 'html');
+        const links = selector.find(html, 'link', true);
+        console.log('got links', links);
       } else {
         throw err;
       }
     });
   } else {
-    console.warn('uage: wld <fileName>');
+    console.warn('usage: wld <fileName>');
     process.exit(1);
   }
 }
